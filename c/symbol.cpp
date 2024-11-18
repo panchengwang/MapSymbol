c09328cf25fbbcafd7432e2117e176fbaa6bbc7e
pcwang （麓山老将） <wang_wang_lao@163.com>
2024-11-18 18:26:31 +0800

    toimage

--- c/symbol.cpp
+++ c/symbol.cpp
@@ -29,14 +29,21 @@ unsigned char* symbol_serialize(HSYMBOL hsym, size_t* len){
     return data;
 }
 
-HSYMBOL symbole_deserialize(unsigned char* data){
+HSYMBOL symbol_deserialize(unsigned char* data){
     SSymbol *sym = new SSymbol();
     sym->deserialize(data);
     return (HSYMBOL)sym;
 }
 
 
-unsigned char* symbol_to_image(HSYMBOL hsym, const char* format, double width, double height, double dotsPerMM, size_t* len){
+unsigned char* symbol_to_image(HSYMBOL hsym, const char* format, double dotsPerMM, size_t* len){
+    SSymbol *sym = (SSymbol*)hsym;
+    unsigned char* data = sym->toImage(format,dotsPerMM,*len);
+    return data;
+}
+
+
+unsigned char* symbol_to_image_with_width_height(HSYMBOL hsym, const char* format, double width, double height, double dotsPerMM, size_t* len){
     SSymbol *sym = (SSymbol*)hsym;
     unsigned char* data = sym->toImage(format,width,height,dotsPerMM,*len);
     return data;
@@ -60,3 +67,25 @@ HSYMBOL symbol_get_shape(HSYMBOL hsym, size_t shpidx)
     SSymbol *sym = (SSymbol*)hsym;
     return (HSYMBOL)sym->clone(shpidx);
 }
+
+
+uint8_t* symbol_to_json_string(HSYMBOL hsym, size_t* len, uint8_t pretty)
+{
+    SSymbol *sym = (SSymbol*)hsym;
+    std::string jsonstr = sym->toJsonString(pretty);
+    *len = jsonstr.size();
+    uint8_t* buf = (uint8_t*)malloc(*len * sizeof(uint8_t));
+    memcpy(buf,jsonstr.c_str(),*len);
+    return buf;
+}
+
+
+uint8_t* symbol_pretty(HSYMBOL hsym, size_t* len){
+    return symbol_to_json_string(hsym,len,true);
+}
+
+
+uint8_t* symbol_compress(HSYMBOL hsym, size_t* len){
+    return symbol_to_json_string(hsym,len,false);
+}
+
--- c/symbol.h
+++ c/symbol.h
@@ -13,13 +13,18 @@ extern "C"
 typedef void* HSYMBOL;
 
 DLL_EXPORT HSYMBOL symbol_from_json_string(const char* jsonstr, uint8_t* ok);
+DLL_EXPORT uint8_t* symbol_to_json_string(HSYMBOL hsym, size_t* len, uint8_t pretty);
+DLL_EXPORT uint8_t* symbol_pretty(HSYMBOL hsym, size_t* len);
+DLL_EXPORT uint8_t* symbol_compress(HSYMBOL hsym, size_t* len);
+
 DLL_EXPORT const char* symbol_get_error_message(HSYMBOL hsym);
 DLL_EXPORT void symbol_destroy(HSYMBOL hsym);
 
 DLL_EXPORT unsigned char* symbol_serialize(HSYMBOL hsym, size_t* len);
-DLL_EXPORT HSYMBOL symbole_deserialize(unsigned char* data);
+DLL_EXPORT HSYMBOL symbol_deserialize(unsigned char* data);
 
-DLL_EXPORT unsigned char* symbol_to_image(HSYMBOL hsym, const char* format, double width, double height, double dotsPerMM, size_t* len);
+DLL_EXPORT unsigned char* symbol_to_image(HSYMBOL hsym, const char* format, double dotsPerMM, size_t* len);
+unsigned char* symbol_to_image_with_width_height(HSYMBOL hsym, const char* format, double width, double height, double dotsPerMM, size_t* len);
 DLL_EXPORT unsigned char* symbol_shape_to_image(HSYMBOL hsym, size_t shpidx, const char* format, double width, double height, double dotsPerMM, size_t* len);
 DLL_EXPORT size_t symbol_nshapes(HSYMBOL hsym);
 DLL_EXPORT HSYMBOL symbol_get_shape(HSYMBOL hsym, size_t shpidx);
